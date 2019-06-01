d5281c581b3fd4d15c475d6db87f862792ba7b92
RobbieCook <robbieck7@gmail.com>
2019-06-01 14:47:08 +1200

    Button improvements

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,6 +11,7 @@ import Dimensions from "../components/Dimensions"
  */
 class Button extends Component {
   render() {
+    /* Styling */
     const customTheme = {
       button: {
         border: {
@@ -18,7 +19,6 @@ class Button extends Component {
         },
       },
     }
-
     const MyButton = styled(GrommetButton)`
       border: 3px solid ${Globals.link.color};
       border-radius: 6px;
@@ -28,13 +28,6 @@ class Button extends Component {
       /* for svg icons */
       fill: ${Globals.text.color};
       stroke: ${Globals.text.color};
-
-      & p {
-        color: inherit;
-        margin: 0 10px;
-        text-decoration: none;
-        border-bottom: 1px solid ${Globals.text.color};
-      }
 
       &:hover {
         & p {
@@ -46,16 +39,34 @@ class Button extends Component {
       background-color: transparent;
       margin: ${this.props.margin ? this.props.margin : new Dimensions(10, 20)};
       color: ${Globals.textColor};
-      line-height: 28px;
-      font-size: 14px;
-      padding: 0 15px;
+      line-height: 27px;
+      font-size: 16px;
+      padding: 5px 12px;
       font-weight: normal;
       ${this.props.style};
     `
 
+    const BorderedText = styled.p`
+      color: ${Globals.textColor};
+      margin: 0;
+      text-decoration: none;
+      border-bottom: 1px solid ${Globals.text.color};
+      line-height: 20px;
+    `
+
+    const innerDom = (
+      <MyButton
+        icon={this.props.icon}
+        label={<BorderedText>{this.props.label}</BorderedText>}
+        onClick={this.props.function}
+        href={this.props.to}
+      />
+    )
+
     return (
       <Grommet theme={customTheme}>
-        <MyButton label={this.props.children} onClick={this.props.function} />
+        {/* Make the button a link if this.props.to is specified */}
+        {innerDom}
       </Grommet>
     )
   }
@@ -64,53 +75,79 @@ Button.propTypes = {
   style: PropTypes.string,
   color: PropTypes.string,
   borderColor: PropTypes.string,
-  function: PropTypes.func.isRequired,
+  function: PropTypes.func,
+  icon: PropTypes.elementType,
+  to: PropTypes.string, // Signifies the button is a link, this is the link address
+  label: PropTypes.string, // Label for the button
 }
 
 /**
  * A group of Buttons, given an array of button titles, and an array of
  * corresponding functions.
  */
-class ButtonGroup extends Component {
+class ButtonGenerator extends Component {
   render() {
-    const ButtonContainer = styled.div`
-      ${(() => {
-        /*
-          Set the flex direction of the container to "row" if 
-          this.props.buttonRow is true.
-        */
-        return (
-          "flex-direction: " + (this.props.buttonRow ? "row" : "column") + ";"
-        )
-      })()}
-      display: flex;
-    `
     return (
       /* 
       Returns buttons, each with a function passed to the button
       as a prop.
       */
-      <ButtonContainer>
+      <ButtonWrapper buttonRow={this.props.buttonRow}>
+        {/* Buttons defined as props */}
         {this.props.titles.map((item, index) => {
           return (
             <Button
               function={this.props.functions[index]}
               margin={this.props.buttonMargin}
               style={this.props.buttonStyle}
-            >
-              {item}
-            </Button>
+              label={item}
+            />
           )
         })}
-      </ButtonContainer>
+        {/* Buttons defined as children */}
+      </ButtonWrapper>
     )
   }
 }
-ButtonGroup.propTypes = {
+ButtonGenerator.propTypes = {
   titles: PropTypes.array.isRequired,
-  functions: PropTypes.array.isRequired, // Should be an array of functions
+  functions: PropTypes.array, // Should be an array of functions
   buttonRow: PropTypes.bool, // Whether to display the buttons as a row
   buttonStyle: PropTypes.string, // Styles passed to the button
 }
 
-export { Button, ButtonGroup }
+/* 
+A wrapper for buttons.
+Used when buttons don't have to be dynamically generated -- can be 
+passed as children
+*/
+class ButtonWrapper extends Component {
+  render() {
+    const ButtonContainer = styled.div`
+      ${(() => {
+        /*
+        Set the flex direction of the container to "row" if 
+        this.props.buttonRow is true.
+      */
+        return (
+          /*
+           * Sets flex direction to row by default, and column if buttonRow
+           * is true
+           */
+          "flex-direction: " +
+          (this.props.buttonRow !== false ? "row" : "column") +
+          ";"
+        )
+      })()}
+      display: flex;
+      ${this.props.style};
+    `
+    return <ButtonContainer>{this.props.children}</ButtonContainer>
+  }
+}
+ButtonWrapper.propTypes = {
+  buttonRow: PropTypes.bool, // Whether or not the buttons should be displayed
+  // as a row or column. Defaults to true.
+}
+
+export { Button, ButtonGenerator, ButtonWrapper }
