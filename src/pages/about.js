725f480dd0b0493921181163133743a1fcf8e9ce
RobbieCook <robbieck7@gmail.com>
2019-06-11 14:49:31 +1200

    Moved Skills subpage out of about to a separate file

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,8 @@ import PropTypes from "prop-types"
 import { Header } from "../components/Typography"
 import Dimensions from "../components/Dimensions"
 import { Button as GrommetButton, Grommet } from "grommet"
-import {Button, ButtonGenerator} from "../components/Button"
+import { Button, ButtonGenerator, ButtonWrapper } from "../components/Button"
+import SkillsPage from "../components/about/SkillsPage"
 
 /* Carousel */
 import "react-responsive-carousel/lib/styles/carousel.min.css"
@@ -56,6 +57,7 @@ class About extends Component {
       height: 100%;
       align-items: center;
     `
+
     return (
       <Page>
         <LayoutWrapper>
@@ -98,52 +100,7 @@ class About extends Component {
           <Heading type="h2">Skills</Heading>
           <Row>
             <Col width="100%">
-              <ButtonGenerator
-                titles={["Development", "General"]}
-                functions={[
-                  () => {
-                    console.log("Button1Pressed")
-                  },
-                  () => {
-                    console.log("Button2Pressed")
-                  },
-                ]}
-                buttonRow={true}
-                buttonStyle="margin: 10px 0 10px 0;"
-              />
-              <SkillsBox
-                title="Web Development"
-                tags={[
-                  "JavaScript",
-                  "React",
-                  "Gatsby",
-                  "VS Code",
-                  "UI Frameworks",
-                ]}
-                backgroundColor="#43DE49"
-                titleColor="#0f0f0f"
-              />
-
-              <SkillsBox
-                title="Microsoft Development"
-                tags={["C#", "VB.NET", "MS-SQL & SSMS"]}
-                backgroundColor="#43DE49"
-                titleColor="#0f0f0f"
-              />
-
-              <SkillsBox
-                title="Development Languages"
-                tags={["JavaScript", "Python", "C#", "VB.NET", "Java"]}
-                backgroundColor="#43DE49"
-                titleColor="#0f0f0f"
-              />
-
-              <SkillsBox
-                title="Other"
-                tags={["bash", "Virtualisation (Hyper-V)", "LaTeX"]}
-                backgroundColor="#43DE49"
-                titleColor="#0f0f0f"
-              />
+              <SkillsPage />
             </Col>
           </Row>
         </LayoutWrapper>
@@ -152,117 +109,5 @@ class About extends Component {
   }
 }
 
-/** The tags of the SkillsBox, which lives inside a SkillsBox */
-class SkillsBoxInfo extends Component {
-  render() {
-    const SkillsBoxInfo = styled.div`
-      height: ${this.props.height};
-      line-height: ${this.props.height};
-      justify-content: center;
-      align-items: center;
-      padding: 0 20px;
-      font-weight: bold;
-      font: inherit;
-
-      @media (max-width: ${Globals.mobile.size}px) {
-        height: auto;
-      }
-    `
-
-    const Tag = styled.span`
-      margin: 0 14px 0 0;
-      display: inline;
-      height: ${this.props.height};
-      font-weight: bold;
-      font: inherit;
-      line-height: ${this.props.height};
-    `
-
-    return (
-      <SkillsBoxInfo>
-        {this.props.tags.map((item, index) => {
-          return (
-            <Tag>
-              {item}
-              {/* Put a comma at the end, except for the last item */}
-              {index !== this.props.tags.length - 1 ? "," : ""}
-            </Tag>
-          )
-        })}
-      </SkillsBoxInfo>
-    )
-  }
-}
-SkillsBoxInfo.propTypes = {
-  tags: PropTypes.string,
-  height: PropTypes.string,
-}
-
-/* A header which lives inside a SkillsBox */
-class SkillsBoxHeader extends Component {
-  render() {
-    const SkillsBoxHead = styled.div`
-      height: ${this.props.height};
-      background-color: ${this.props.backgroundColor};
-      color: ${this.props.titleColor}!important;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 0 20px;
-      line-height: ${this.props.height};
-    `
-    return (
-      <SkillsBoxHead>
-        <Heading type="h3" style={{ margin: "0px", fontWeight: "normal" }}>
-          {this.props.title}
-        </Heading>
-      </SkillsBoxHead>
-    )
-  }
-}
-SkillsBoxHeader.propTypes = {
-  titleColor: PropTypes.string,
-  backgroundColor: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  height: PropTypes.string.isRequired,
-}
-
-/** Container for skills info */
-class SkillsBox extends Component {
-  render() {
-    let height = this.props.height === undefined ? "50px" : this.props.height
-
-    const SkillsBox = styled.div`
-      border: 1px solid #656565;
-      height: ${height};
-      width: 100%;
-      display: flex;
-      ${this.props.styles};
-      margin-bottom: 20px;
-
-      @media (max-width: ${Globals.mobile.size}px) {
-        height: auto;
-        flex-direction: column;
-      }
-    `
-
-    return (
-      <SkillsBox>
-        <SkillsBoxHeader {...this.props} height={height}>
-          {this.props.children}
-        </SkillsBoxHeader>
-        <SkillsBoxInfo {...this.props} height={height} />
-      </SkillsBox>
-    )
-  }
-}
-SkillsBox.propTypes = {
-  styles: PropTypes.string,
-  height: PropTypes.string,
-  titleColor: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  tags: PropTypes.array.isRequired,
-  backgroundColor: PropTypes.string.isRequired,
-}
 
 export default About
