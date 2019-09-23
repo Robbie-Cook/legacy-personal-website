6a52b3687ae0fdefe1e70b3240ed7f0e547f661f
Robbie Cook <robbie@cloudcannon.com>
2019-09-23 18:21:04 +1200

    Changed indent

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
-        resolve: `gatsby-plugin-nprogress`
+      resolve: `gatsby-plugin-nprogress`
     },
     {
       resolve: `gatsby-plugin-manifest`,
