f6edc72ee22913b5c3b0517ae690e38183a1b531
Robbie Cook <robbieck7@gmail.com>
2019-09-28 20:42:43 +1200

    Adding image packages to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,28 @@
 module.exports = {
-
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         name: `carousel`,
         path: `${__dirname}/src/photos/carousels`,
       },
     },
-    `gatsby-plugin-transition-link`,
     `gatsby-plugin-material-ui`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     {
-      resolve: `gatsby-plugin-nprogress`
+      resolve: `gatsby-plugin-nprogress`,
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Robbie Cook`,
         short_name: `Robbie Cook`,
-        icon: `src/photos/icon.png`
+        icon: `src/photos/icon.png`,
       },
     },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
   ],
-}
+};
